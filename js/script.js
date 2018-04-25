fdf38bc27f2d9cba71e70d851ddc036f48892f2a
Marcin Świrek <swirek.marcin@gmail.com>
2018-04-26 00:21:16 +0200

    Add script in .js file

---
+++ js/script.js
@@ -0,0 +1,41 @@
+$(function() {
+  var tweetLink = "https://twitter.com/intent/tweet?text=";
+  var quoteUrl =
+    "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+  var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+
+  function getQuote() {
+    $getJSON(quoteUrl, createTweet);
+  }
+
+  function createTweet(input) {
+    var data = input[0];
+
+    var quoteText = $(data.content)
+      .text()
+      .trim();
+    var quoteAuthor = data.title;
+
+    if (!quoteAuthor.length) {
+      quoteAuthor = "Unknown author";
+    }
+
+    if (tweetText.length > 140) {
+      getQuote();
+    } else {
+      var tweet = tweetLink + encodeURIComponent(tweetText);
+      $(".quote").text(quoteText);
+      $(".author").text("Author: " + quoteAuthor);
+      $(".tweet").attr("href", tweet);
+    }
+
+    $(".tweet").attr("href", tweet);
+  }
+});
+
+$(document).ready(function() {
+  getQuote();
+  $(".trigger").click(function() {
+    getQuote();
+  });
+});
