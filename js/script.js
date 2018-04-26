a1beee993ac26ce2c86fe713bba0df6389459f93
Marcin Świrek <swirek.marcin@gmail.com>
2018-04-26 21:46:27 +0200

    Fix js script

--- js/script.js
+++ js/script.js
@@ -1,41 +1,84 @@
-$(function() {
-  var tweetLink = "https://twitter.com/intent/tweet?text=";
-  var quoteUrl =
-    "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+// $(function() {
+//   var tweetLink = "https://twitter.com/intent/tweet?text=";
+//   var quoteUrl =
+//     "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+//   var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+
+//   function getQuote() {
+//     $getJSON(quoteUrl, createTweet);
+//   }
+
+//   function createTweet(input) {
+//     var data = input[0];
+
+//     var quoteText = $(data.content)
+//       .text()
+//       .trim();
+//     var quoteAuthor = data.title;
+
+//     if (!quoteAuthor.length) {
+//       quoteAuthor = "Unknown author";
+//     }
+
+//     if (tweetText.length > 140) {
+//       getQuote();
+//     } else {
+//       var tweet = tweetLink + encodeURIComponent(tweetText);
+//       $(".quote").text(quoteText);
+//       $(".author").text("Author: " + quoteAuthor);
+//       $(".tweet").attr("href", tweet);
+//     }
+
+//     $(".tweet").attr("href", tweet);
+//   }
+
+//   $(document).ready(function() {
+//     getQuote();
+//     $(".trigger").click(function() {
+//       getQuote();
+//     });
+//   });
+// });
+
+// Z pomoca
+
+var tweetLink = "https://twitter.com/intent/tweet?text=";
+var prefix = "https://cors-anywhere.herokuapp.com/";
+var quoteUrl =
+  "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+
+function getQuote() {
+  $.getJSON(prefix + quoteUrl, createTweet);
+  $.ajaxSetup({ cache: false });
+}
+
+function createTweet(input) {
+  var data = input[0];
+  var quoteText = $(data.content)
+    .text()
+    .trim();
+  var quoteAuthor = data.title;
+
+  if (!quoteAuthor.length) {
+    quoteAuthor = "Unknown author";
+  }
+
   var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
 
-  function getQuote() {
-    $getJSON(quoteUrl, createTweet);
-  }
-
-  function createTweet(input) {
-    var data = input[0];
-
-    var quoteText = $(data.content)
-      .text()
-      .trim();
-    var quoteAuthor = data.title;
-
-    if (!quoteAuthor.length) {
-      quoteAuthor = "Unknown author";
-    }
-
-    if (tweetText.length > 140) {
-      getQuote();
-    } else {
-      var tweet = tweetLink + encodeURIComponent(tweetText);
-      $(".quote").text(quoteText);
-      $(".author").text("Author: " + quoteAuthor);
-      $(".tweet").attr("href", tweet);
-    }
-
+  if (tweetText.length > 140) {
+    getQuote();
+  } else {
+    var tweet = tweetLink + encodeURIComponent(tweetText);
+    $(".quote").text(quoteText);
+    $(".author").text("Author: " + quoteAuthor);
     $(".tweet").attr("href", tweet);
   }
+}
 
-  $(document).ready(function() {
+$(document).ready(function() {
+  getQuote();
+
+  $(".trigger").click(function() {
     getQuote();
-    $(".trigger").click(function() {
-      getQuote();
-    });
   });
 });
